75c92364f753c573d5102754a1b3d8addfb15735
Yash Pandey <yashpandey1556@gmail.com>
2026-06-01 15:13:38 +0530

    added requirements / swaped to postgres from sqlite and better envinronment variables handling

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./notes.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind = engine)
 Base = declarative_base()
